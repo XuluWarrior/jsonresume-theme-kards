d6b042bdcc86130717e3ca802dd557a5cabb6313
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2023-04-04 01:33:51 +0200

    Make copyright dynamic

--- index.js
+++ index.js
@@ -137,7 +137,25 @@ function render(resume) {
 	//});
 
 	const packageJSON = require("./package");
-	return Handlebars.compile(tpl)({
+
+    Handlebars.registerHelper('now', function(context) {
+        const format = context;
+        const date = new Date();
+        const pad2 = (n) => n.toString().padStart(2, '0');
+
+        const map = {
+          YYYY: date.getFullYear(),
+          MM: pad2(date.getMonth() + 1),
+          DD: pad2(date.getDate()),
+          hh: pad2(date.getHours()),
+          mm: pad2(date.getMinutes()),
+          ss: pad2(date.getSeconds()),
+        };
+
+        return Object.entries(map).reduce((prev, entry) => prev.replace(...entry), format);
+    });
+
+    return Handlebars.compile(tpl)({
 		css: css,
 		mainJs: mainJs,
 		pluginsJs: pluginsJs,
