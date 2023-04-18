7541d9d17b8b37bc546eb6ed2732d6252f1f6631
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2023-04-18 23:26:31 +0200

    Format and clean up main.js

--- Kards10/js/main.js
+++ Kards10/js/main.js
@@ -1,291 +1,214 @@
-/** 
+/**
  * ===================================================================
  * main js
  *
- * ------------------------------------------------------------------- 
- */ 
+ * -------------------------------------------------------------------
+ */
 
-(function($) {
+(function ($) {
 
-	"use strict";
+    "use strict";
 
-	/*---------------------------------------------------- */
-	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
+    /*---------------------------------------------------- */
+    /* Preloader
+    ------------------------------------------------------ */
+    $(window).load(function () {
 
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
+        // will first fade out the loading animation
+        $("#loader").fadeOut("slow", function () {
 
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(300).fadeOut("slow");
 
-      });       
+        });
 
-  	})
-
-
-  	/*---------------------------------------------------- */
-  	/* FitText Settings
-  	------------------------------------------------------ */
-  	setTimeout(function() {
-
-   	$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
-
-  	}, 100);
+    })
 
 
-	/*---------------------------------------------------- */
-	/* FitVids
-	------------------------------------------------------ */ 
-  	$(".fluid-video-wrapper").fitVids();
+    /*---------------------------------------------------- */
+    /* FitText Settings
+    ------------------------------------------------------ */
+    setTimeout(function () {
+
+        $('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
+
+    }, 100);
 
 
-	/*---------------------------------------------------- */
-	/* Owl Carousel
-	------------------------------------------------------ */ 
-	$("#owl-slider").owlCarousel({
+    /*---------------------------------------------------- */
+    /* FitVids
+    ------------------------------------------------------ */
+    $(".fluid-video-wrapper").fitVids();
+
+
+    /*---------------------------------------------------- */
+    /* Owl Carousel
+    ------------------------------------------------------ */
+    $("#owl-slider").owlCarousel({
         navigation: false,
         pagination: true,
-        itemsCustom : [
-	        [0, 1],
-	        [700, 2],
-	        [960, 3]
-	     ],
+        itemsCustom: [
+            [0, 1],
+            [700, 2],
+            [960, 3]
+        ],
         navigationText: false
     });
 
 
-	/*----------------------------------------------------- */
-	/* Alert Boxes
-  	------------------------------------------------------- */
-	$('.alert-box').on('click', '.close', function() {
-	  $(this).parent().fadeOut(500);
-	});	
+    /*----------------------------------------------------- */
+    /* Alert Boxes
+        ------------------------------------------------------- */
+    $('.alert-box').on('click', '.close', function () {
+        $(this).parent().fadeOut(500);
+    });
 
 
-	/*----------------------------------------------------- */
-	/* Stat Counter
-  	------------------------------------------------------- */
-   var statSection = $("#stats"),
-       stats = $(".stat-count");
+    /*----------------------------------------------------- */
+    /* Stat Counter
+        ------------------------------------------------------- */
+    var statSection = $("#stats"),
+        stats = $(".stat-count");
 
-   statSection.waypoint({
+    statSection.waypoint({
 
-   	handler: function(direction) {
+        handler: function (direction) {
 
-      	if (direction === "down") {       		
+            if (direction === "down") {
 
-			   stats.each(function () {
-				   var $this = $(this);
+                stats.each(function () {
+                    var $this = $(this);
 
-				   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-				   	duration: 4000,
-				   	easing: 'swing',
-				   	step: function (curValue) {
-				      	$this.text(Math.ceil(curValue));
-				    	}
-				  	});
-				});
+                    $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                        duration: 4000,
+                        easing: 'swing',
+                        step: function (curValue) {
+                            $this.text(Math.ceil(curValue));
+                        }
+                    });
+                });
 
-       	} 
+            }
 
-       	// trigger once only
-       	this.destroy();      	
+            // trigger once only
+            this.destroy();
 
-		},
-			
-		offset: "90%"
-	
-	});	
+        },
 
+        offset: "90%"
 
-	/*---------------------------------------------------- */
-	/*	Masonry
-	------------------------------------------------------ */
-	var containerProjects = $('#folio-wrapper');
+    });
 
-	containerProjects.imagesLoaded( function() {
 
-		containerProjects.masonry( {		  
-		  	itemSelector: '.folio-item',
-		  	resize: true 
-		});
+    /*---------------------------------------------------- */
+    /*	Masonry
+    ------------------------------------------------------ */
+    var containerProjects = $('#folio-wrapper');
 
-	});
+    containerProjects.imagesLoaded(function () {
 
+        containerProjects.masonry({
+            itemSelector: '.folio-item',
+            resize: true
+        });
 
-	/*----------------------------------------------------*/
-	/*	Modal Popup
-	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
+    });
 
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
 
-   });
+    /*----------------------------------------------------*/
+    /*	Modal Popup
+    ------------------------------------------------------*/
+    $('.item-wrap a').magnificPopup({
 
-   $(document).on('click', '.popup-modal-dismiss', function (e) {
-   	e.preventDefault();
-   	$.magnificPopup.close();
-   });
+        type: 'inline',
+        fixedContentPos: false,
+        removalDelay: 300,
+        showCloseBtn: false,
+        mainClass: 'mfp-fade'
 
-	
-	/*-----------------------------------------------------*/
-  	/* Navigation Menu
-   ------------------------------------------------------ */  
-   var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
+    });
 
-   // toggle button
-   toggleButton.on('click', function(e) {
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+    });
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
 
-	});
+    /*-----------------------------------------------------*/
+    /* Navigation Menu
+------------------------------------------------------ */
+    var toggleButton = $('.menu-toggle'),
+        nav = $('.main-navigation');
 
-   // nav items
-  	nav.find('li a').on("click", function() {   
+    // toggle button
+    toggleButton.on('click', function (e) {
 
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
-  	});
+        e.preventDefault();
+        toggleButton.toggleClass('is-clicked');
+        nav.slideToggle();
 
+    });
 
-   /*---------------------------------------------------- */
-  	/* Highlight the current section in the navigation bar
-  	------------------------------------------------------ */
-	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
+    // nav items
+    nav.find('li a').on("click", function () {
 
-	sections.waypoint( {
+        // update the toggle button
+        toggleButton.toggleClass('is-clicked');
+        // fadeout the navigation panel
+        nav.fadeOut();
 
-       handler: function(direction) {
+    });
 
-		   var active_section;
 
-			active_section = $('section#' + this.element.id);
+    /*---------------------------------------------------- */
+    /* Highlight the current section in the navigation bar
+    ------------------------------------------------------ */
+    var sections = $("section"),
+        navigation_links = $("#main-nav-wrap li a");
 
-			if (direction === "up") active_section = active_section.prev();
+    sections.waypoint({
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+        handler: function (direction) {
 
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
+            var active_section;
 
-		}, 
+            active_section = $('section#' + this.element.id);
 
-		offset: '25%'
-	});
+            if (direction === "up") active_section = active_section.prev();
 
+            var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
-	/*---------------------------------------------------- */
-  	/* Smooth Scrolling
-  	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
+            navigation_links.parent().removeClass("current");
+            active_link.parent().addClass("current");
 
-   	var target = this.hash,
-    	$target = $(target);
+        },
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+        offset: '25%'
+    });
 
-  	});  
-  
 
-   /*---------------------------------------------------- */
-	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */ 
-	$('input, textarea, select').placeholder()  
 
+    /*----------------------------------------------------- */
+    /* Back to top
+------------------------------------------------------- */
+    var pxShow = 300; // height on which the button will show
+    var fadeInTime = 400; // how slow/fast you want the button to show
+    var fadeOutTime = 400; // how slow/fast you want the button to hide
+    var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
-  	/*---------------------------------------------------- */
-	/*	contact form
-	------------------------------------------------------ */
+    // Show or hide the sticky footer button
+    jQuery(window).scroll(function () {
 
-	/* local validation */
-	$('#contactForm').validate({
+        if (!($("#header-search").hasClass('is-visible'))) {
 
-		/* submit via ajax */
-		submitHandler: function(form) {
+            if (jQuery(window).scrollTop() >= pxShow) {
+                jQuery("#go-top").fadeIn(fadeInTime);
+            } else {
+                jQuery("#go-top").fadeOut(fadeOutTime);
+            }
 
-			var sLoader = $('#submit-loader');
+        }
 
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
-	});
-
-
- 	/*----------------------------------------------------- */
-  	/* Back to top
-   ------------------------------------------------------- */ 
-	var pxShow = 300; // height on which the button will show
-	var fadeInTime = 400; // how slow/fast you want the button to show
-	var fadeOutTime = 400; // how slow/fast you want the button to hide
-	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
-
-   // Show or hide the sticky footer button
-	jQuery(window).scroll(function() {
-
-		if (!( $("#header-search").hasClass('is-visible'))) {
-
-			if (jQuery(window).scrollTop() >= pxShow) {
-				jQuery("#go-top").fadeIn(fadeInTime);
-			} else {
-				jQuery("#go-top").fadeOut(fadeOutTime);
-			}
-
-		}		
-
-	});		
+    });
 
 })(jQuery);
