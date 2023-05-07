48b20fcd88a92593a974522a20bc98b039f3ea63
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2023-05-07 23:21:24 +0200

    Make percy a development dependency
    - Install percy as a development dependency instead of installing it
      as a separate build step.
    - [Replace deprecated `waitFor` with `waitForTimeout`][1].
    - Explicitly add `--theme .` argument to the `npx resume` command.
    
    [1]: https://github.com/puppeteer/puppeteer/issues/6214

--- tests/percy.js
+++ tests/percy.js
@@ -2,10 +2,10 @@ const PercyScript = require('@percy/script');
 
 async function clickMenuLink(page, percySnapshot, linkName) {
     await page.click('[data-test=menu-toggle]');
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
 
     await page.click(`[data-test=${linkName}-link]`);
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
 
     // percy actualy takes fullscreen snapshots.  So after click, snapshot will be identical to homepage one
     // TODO: find different way to test scroll behaviour
@@ -14,17 +14,17 @@ async function clickMenuLink(page, percySnapshot, linkName) {
 
 PercyScript.run(async (page, percySnapshot) => {
     await page.goto('http://localhost:5001/index.html');
-    await page.waitFor(".pace-inactive");
+    await page.waitForTimeout(".pace-inactive");
     await percySnapshot('homepage');
 
     // Open menu
     await page.click('[data-test=menu-toggle]');
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
     await percySnapshot('menu');
 
     // Close menu
     await page.click('[data-test=menu-toggle]');
-    await page.waitFor(1000);
+    await page.waitForTimeout(1000);
 
     await clickMenuLink(page, percySnapshot, 'about');
 });
